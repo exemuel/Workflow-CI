2b584c367ec672f042ec1e934cbbf30eb5e74ae3
Samuel Situmeang <samsitumeang@gmail.com>
2026-07-13 22:03:04 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,10 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import mlflow
+import os
 
 def main():
-    # Set up MLflow tracking locally
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment("Titanic_Classification_Basic")
     
     # Enable autolog
@@ -14,16 +13,22 @@ def main():
     # Load data
     df = pd.read_csv("titanic_preprocessing/titanic_cleaned.csv")
     
-    # Assuming 'Survived' is the target column
     X = df.drop(columns=['Survived'])
     y = df['Survived']
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    with mlflow.start_run(run_name="RandomForest_Basic"):
+    active_run = mlflow.active_run()
+    if active_run:
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
-        print("Model training completed with autologging.")
+        print(f"Model training completed within active run: {active_run.info.run_id}")
+    else:
+        with mlflow.start_run(run_name="RandomForest_Basic"):
+            model = RandomForestClassifier(n_estimators=100, random_state=42)
+            model.fit(X_train, y_train)
+            print("Model training completed with autologging.")
 
 if __name__ == "__main__":
     main()
+
